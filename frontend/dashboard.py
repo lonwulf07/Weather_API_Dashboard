9758be9a70d17156b933e3a6c9d189ec224e1325
Mohit <smsdenelumi@gmail.com>
2026-02-03 17:21:11 +0530

    Updated API URL for production

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -10,7 +10,7 @@ st.set_page_config(
 
 st.title("🌦 Weather Analytics & ML Forecast Platform")
 
-API_BASE = "http://127.0.0.1:8000"
+API_BASE = "https://weather-api-backend.onrender.com"
 
 # ------------------ CURRENT WEATHER ------------------
 
